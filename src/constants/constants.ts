eaa77e214ad97f872a4395cec3c249d1da3ecff6
Denis <serzhantov-02@mail.ru>
2022-08-31 22:30:19 +0500

    env_data: add environment variables and hid the API key

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,6 @@
 
-export const API_KEY = "dcdbea88-8c9c-4f9c-9ef4-bc55c1597048";
-/*export const API_KEY = "c9b8bd8d-0691-44ab-86ef-ec16b61c44f9";*/
-export const API_URL = "https://kinopoiskapiunofficial.tech/api/";
+export const API_KEY = process.env.REACT_APP_API_KEY || "";
+export const API_URL = process.env.REACT_APP_API_URL || "";
 
 
 
