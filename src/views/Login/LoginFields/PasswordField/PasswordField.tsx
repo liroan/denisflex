2ee74a4c63a8ded61f498186cdc4e4205629d964
Denis <serzhantov-02@mail.ru>
2022-09-11 21:01:43 +0500

    login: I add hiding and opening the password field

--- src/views/Login/LoginFields/PasswordField/PasswordField.tsx
+++ src/views/Login/LoginFields/PasswordField/PasswordField.tsx
@@ -1,8 +1,11 @@
 import {REQUIRED} from "../../../../constants";
 import {TextField} from "@mui/material";
 import {Control, Controller, FieldError, FieldErrorsImpl, FieldValues, Merge} from "react-hook-form";
-import React, {FC} from "react";
+import React, {FC, useState} from "react";
 import {getTextLengthError} from "../../../../utils";
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import styles from "./PasswordField.module.scss"
 
 interface PasswordFieldProps {
     name: string;
@@ -12,22 +15,32 @@ interface PasswordFieldProps {
     passwordsError?: string;
 }
 
-const PasswordField:FC<PasswordFieldProps> = ({ name, control, passwordsError, message, placeholder }) => {
+const PasswordField:FC<PasswordFieldProps> = ({ name, control, passwordsError,
+                                                  message, placeholder }) => {
+    const [isShowPassword, setIsShowPassword] = useState(false);
     return (
-        <Controller
-            name={name}
-            control={control}
-            rules={{
-                required: REQUIRED,
-                minLength: { value: 6, message: getTextLengthError("пароля", 6, "не меньше") }
-            }}
-            render={({ field }) => (
-                <TextField {...field} id="outlined-basic" label="Пароль" variant="outlined"
-                           placeholder={placeholder}
-                           error={!!message || !!passwordsError} helperText={(message || passwordsError || "") as string}
-                />
-            )}
-        />
+        <div className={styles.password}>
+            <Controller
+                name={name}
+                control={control}
+                rules={{
+                    required: REQUIRED,
+                    minLength: { value: 6, message: getTextLengthError("пароля", 6, "не меньше") }
+                }}
+                render={({ field }) => (
+                    <TextField {...field} id="outlined-basic" label="Пароль" variant="outlined"
+                               type={isShowPassword ? "text" : "password" }
+                               placeholder={placeholder}
+                               error={!!message || !!passwordsError} helperText={(message || passwordsError || "") as string}
+                    />
+                )}
+            />
+            {
+                isShowPassword ?
+                    <VisibilityOffIcon onClick={() => setIsShowPassword(false)} /> :
+                    <RemoveRedEyeIcon onClick={() => setIsShowPassword(true)} />
+            }
+        </div>
     )
 }
 export default PasswordField;
